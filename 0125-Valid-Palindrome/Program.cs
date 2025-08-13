49b40a7c5fbd4cff384033e0d8b704267055fd25
acurzon <adam@curzon.org>
2025-08-13 23:47:34 +0100

    feat: 0125-Valid-Palindrome

---
+++ 0125-Valid-Palindrome/Program.cs
@@ -0,0 +1,45 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        List<char> letters = new();
+        int left = 0;
+        int right = 0;
+        foreach (char letter in s)
+            if (Char.IsLetterOrDigit(letter))
+                letters.Add(Char.ToLower(letter));
+
+        if (letters.Count % 2 == 0)
+        {
+            left = letters.Count / 2 - 1;
+            right = letters.Count / 2;
+            while (left >= 0 && right < letters.Count)
+            {
+                if (letters[left] != letters[right]) return false;
+                left--;
+                right++;
+            }
+        }
+        else
+        {
+            left = letters.Count / 2 + 1;
+            right = letters.Count / 2 - 1;
+            while (left >= 0 && right < letters.Count)
+            {
+                if (letters[left] != letters[right]) return false;
+                left--;
+                right++;
+            }
+        }
+        return true;
+    }
+
+    static void Main()
+    {
+        Solution s = new();
+        Console.WriteLine(s.IsPalindrome("A man, a plan, a canal: Panama"));
+        Console.WriteLine(s.IsPalindrome("racecar"));
+        Console.WriteLine(s.IsPalindrome("raceecar"));
+        Console.WriteLine(s.IsPalindrome("0P"));
+    }
+}
