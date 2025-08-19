254b02ba4529eb0b91e3a5aea08375a9518a0de3
acurzon <adam@curzon.org>
2025-08-19 15:59:51 +0100

    0121-Best-Time-To-Buy-And-Sell-Stock

---
+++ 0121-Best-Time-To-Buy-And-Sell-Stock/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int lBuy = 0;
+        int rSell = 1;
+        int profit = 0;
+
+        while (rSell < prices.Length)
+        {
+            if (prices[lBuy] < prices[rSell])
+            {
+                profit = Math.Max(profit, prices[rSell] - prices[lBuy]);
+            }
+            else
+            {
+                lBuy = rSell;
+            }
+            rSell++;
+        }
+
+        return profit;
+    }
+
+    static void Main()
+    {
+        Solution s = new();
+        Console.WriteLine(s.MaxProfit([7, 1, 5, 3, 6, 4]));
+        Console.WriteLine(s.MaxProfit([7, 6, 4, 3, 1]));
+    }
+}
